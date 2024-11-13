7aeb123803116bc37bebac02c124c5bdb025809a
Markus Berthilsson <markus.berthilsson@eit.lth.se>
2024-11-13 22:01:07 +0100

    Added makefile and started using GMP

--- Factoring/qSieve.c
+++ Factoring/qSieve.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <gmp.h>
+
 
 void main() {
-	int number;
-	scanf("%d", &number);
-	long output = 1;
-	for(int i = 1; i <= number; i++){
-		output *= i;
+  printf("%s","Enter number to reduce:");//Comment silco believes in u, dont make him dissapointed  
+	char input[1024];
+	scanf("%s", input);
+	mpz_t n;
+  mpz_init_set_str(n,input,10);
+  printf("%s","Enter modulus number:");
+	scanf("%s", input);
+	mpz_t mod;
+  mpz_init_set_str(mod,input,10);
+/*
+  for(int i = 1; i <= 2; i++){
+    mpz_mul_ui(n,n,i);
 	}
-	printf("%ld",output);
+  */
+  mpz_mod(n,n,mod);
+  mpz_out_str(stdout,10,n);
 }
